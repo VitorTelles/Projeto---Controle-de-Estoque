499d3fe2c00ec77696c58de80625060837b0dcd6
Vitor Telles <vitortelles120599@gmail.com>
2021-11-02 20:48:37 -0300

    alteração na classe painel.php

--- classes/Painel.php
+++ classes/Painel.php
@@ -5,16 +5,17 @@
         Public static function logado(){
             return isset($_SESSION['login']) ? true : false;
         }
-     
+
         public static function logout(){
             session_destroy();
             header('Location: '.INCLUDE_PATH);
         }
         
         public static function redirect($url){
-			echo '<script>location.href="'.$url.'"</script>';
-			die();
-		}
+            echo '<script>location.href="'.$url.'"</script>';
+            die();
+        }
+        
     }
 
 ?>
