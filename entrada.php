47b8680a2760c67130e31b4637351f7f1f0bca48
steim01 <dev.steim01@gmail.com>
2021-11-02 18:53:14 -0300

    criado a pagina de entrada de itens do estoque

--- entrada.php
+++ entrada.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,10 +8,24 @@
     <link rel="stylesheet" type="text/css" href="./styles/entrada.css">
     <title>Entrada de itens</title>
 </head>
-<body>
-    <header>
 
-    </header>
+<body>
+    <div class="left_menu">
+        <nav>
+            <div class="logo">
+                <img src="./imagens/xerife_icon.png">
+            </div>
+
+            <ul>
+                
+                <li>
+                    <p>Voltar</p>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
 
 </body>
+
 </html>
