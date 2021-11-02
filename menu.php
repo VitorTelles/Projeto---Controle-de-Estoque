2e34ca8523a6e72dab9b4d55b37a4e4926ca76b3
Vitor Telles <vitortelles120599@gmail.com>
2021-11-02 15:29:58 -0300

    Estilos da tela de Menu

---
+++ menu.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+    <title>Controle de Estoque</title>
+    <link rel="stylesheet" href="./styles/menu.css">
+    </head>
+    <!-- Opções do Menu-->
+    <body>
+        <nav>
+            <div class="logo">
+                <h1>Almoxerife</h1>
+            </div>
+            <button>&#9776;</button>
+            
+            <ul>
+                <li>
+                    <p>Cadastro de Produtos</p>
+                </li>
+                <li>
+                    <p>Entrada de Produtos</p>
+                </li>
+                <li>
+                    <p>Saída de Produtos</p>
+                </li>
+                <li>
+                    <p>Sair</p>
+                </li>
+            </ul>
+        </nav>
+    </body>
+</html>
