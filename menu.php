3100de84286861c7659452034b499a4a6658c0b1
steim01 <dev.steim01@gmail.com>
2021-11-02 16:31:44 -0300

    complemento do html feito na pagina do menu.php

--- menu.php
+++ menu.php
@@ -1,7 +1,11 @@
-<html>
-    <head>
-    <title>Controle de Estoque</title>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./styles/menu.css">
+    <title>Controle de Estoque</title>
     </head>
     <!-- Opções do Menu-->
     <body>
