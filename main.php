2c2950f5c9ff39867187ba0d5d8df051a4c107ee
VitorTelles <vitortelles120599@gmail.com>
2022-04-11 17:26:14 -0300

    Alterações css do main

--- main.php
+++ main.php
@@ -13,6 +13,7 @@
     <script src="https://kit.fontawesome.com/f3997679a4.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="./styles/main.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,300" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
     <title>Controle de Estoque</title>
     </head>
     <!-- MENU DE OPÇÕES-->
@@ -28,7 +29,7 @@
                         </div><!--avatar-usuario-->
                     <?php }else{ ?>
                         <div class="imagem-usuario">
-                            <img src="<?php echo INCLUDE_PATH?>?imagens/<?php echo $_SESSION['img'];?>"/>
+                            <img src="<?php echo INCLUDE_PATH?>imagens/<?php echo $_SESSION['img'];?>"/>
                         </div><!--avatar-usuario-->
                     <?php } ?>
                     <div class="nome-usuario">
@@ -37,18 +38,18 @@
                     </div>
                 </div><!--box-usuario-->
                 <div class="items-menu">
-                    <h2>Cadastros</h2>
+                    <h2>Cadastros  <i class="bi bi-bag-plus"></i></h2>
                     <a href="">Cadastrar Produtos</a>
                     <a href="">Cadastrar Clientes</a>
-                    <h2>Consultas</h2>
+                    <h2>Consultas  <i class="bi bi-search"></i></h2>
                     <a href="">Consultar Produtos</a>
                     <a href="">Listar Clientes</a>
-                    <h2>Relatórios</h2>
-                    <a href="">Emitir Relatório</a>
-                    <h2>Movimentos</h2>
+                    <h2>Relatórios  <i class="bi bi-clipboard2-data"></i></h2>
+                    <a href="">Emitir Relatório   </a>
+                    <h2>Movimentos  <i class="bi bi-arrow-down-up"></i></h2>
                     <a href="">Entrada no Estoque</a>
                     <a href="">Saída no Estoque</a>
-                    <h2>Administração do Painel</h2>
+                    <h2>Administração do Painel  <i class="bi bi-gear"></i></h2>
                     <a href="">Inserir novo usuário</a>
                     <a href="">Editar usuário</a>
                 </div><!--items-menu-->
@@ -70,16 +71,17 @@
 
         <!--INICIO DO CONTEUDO DO PAINEL-->
 
-        <div class="content left w100">
-             <h2><i class="fa fa-home">Links Rápidos do Painel</i></h2>
-
-             <div class="box-rapidos">
-                  <div class="box-rapidos-single">
-                      <div class="box-rapidos-wraper"> 
-                        <h2>Saída de produtos</h2>
-                      </div><!--box-rapidos-wraper-->
-                  </div><!--box-rapidos-single-->  
-             </div><!--box-rapidos-->
+        <div class="content left">
+            <div class="box-content">
+            <h2><i class="fa fa-home"></i> Atalhos </h2>
+                <div class="box-rapidos">
+                    <div class="box-rapidos-single">
+                        <div class="box-rapidos-wraper"> 
+                            <h2>Saída de produtos</h2>
+                        </div><!--box-rapidos-wraper-->
+                    </div><!--box-rapidos-single-->  
+                </div><!--box-rapidos-->
+            </div>
              <div class="clear"></div>
         </div><!--content-->
 
